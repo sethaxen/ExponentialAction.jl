2579b81e3a5ce4da6c261b3a2780c4a6ec0af9d1
Seth Axen <seth.axen@gmail.com>
2021-03-17 16:43:53 -0700

    Don't differentiate through _opnormInf (#5)
    * Mark _opnormInf as non-differentiable
    
    * Test non-differentiability of _opnormInf
    
    * Increment version number
    
    * Fix test
    
    * Fix test

--- test/util.jl
+++ test/util.jl
@@ -1,4 +1,4 @@
-using ExponentialAction, LinearAlgebra, Test
+using ExponentialAction, ChainRulesCore, LinearAlgebra, Test
 
 @testset "Utilities" begin
     @testset "_opnormInf" begin
@@ -8,6 +8,17 @@ using ExponentialAction, LinearAlgebra, Test
         @test ExponentialAction._opnormInf(A) ≈ norm(A, Inf)
         A = randn(ComplexF64, 10, 10)
         @test ExponentialAction._opnormInf(A) ≈ opnorm(A, Inf)
+    end
+
+    @testset "_opnormInf non-differentiable" begin
+        A = randn(ComplexF64, 10)
+        n, back = ChainRulesCore.rrule(ExponentialAction._opnormInf, A)
+        @test n == ExponentialAction._opnormInf(A)
+        @test @inferred(back(1.0)) === (DoesNotExist(), DoesNotExist())
+        A = randn(ComplexF64, 10, 10)
+        n, back = ChainRulesCore.rrule(ExponentialAction._opnormInf, A)
+        @test n == ExponentialAction._opnormInf(A)
+        @test @inferred(back(1.0)) === (DoesNotExist(), DoesNotExist())
     end
 
     @testset "opnormest1" begin
