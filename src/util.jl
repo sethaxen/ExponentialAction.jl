9daf879326e7eefb2d06295ce0563c0799d87faa
Seth Axen <seth.axen@gmail.com>
2021-03-17 16:09:19 -0700

    Mark _opnormInf as non-differentiable

--- src/util.jl
+++ src/util.jl
@@ -2,6 +2,9 @@
 _opnormInf(B) = opnorm(B, Inf)
 _opnormInf(B::AbstractVector) = norm(B, Inf)
 
+# we only use _opnormInf for control flow, so avoid differentiating through it
+ChainRulesCore.@non_differentiable _opnormInf(B)
+
 # TODO: replace with estimate
 # see https://github.com/JuliaLang/julia/pull/39058
 opnormest1(A) = opnorm(A, 1)
