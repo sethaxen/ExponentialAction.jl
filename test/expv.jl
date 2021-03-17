44ec6fec943ebf9a2b4b6e9bef3455cdc494c453
Seth Axen <seth.axen@gmail.com>
2021-03-17 15:37:25 -0700

    Add tests for SparseArrays, Bidiagonal, and Tridiagonal

--- test/expv.jl
+++ test/expv.jl
@@ -1,4 +1,4 @@
-using ExponentialAction
+using ExponentialAction, LinearAlgebra, SparseArrays, Test
 
 Tsingle = (Float32, ComplexF32)
 Tdouble = (Float64, ComplexF64)
@@ -6,7 +6,7 @@ Tdouble = (Float64, ComplexF64)
 @testset "expv" begin
     n = 10
     @testset "expv(t::$Tt, A::$MT{$TA}, B::Array{$TB,$(length(Bdims2)+1)}), tscale=$tscale, shift=$shift" for
-        MT in (Matrix, Diagonal),
+        MT in (Matrix, Diagonal, Bidiagonal, Tridiagonal),
         Tset in (Tsingle, Tdouble),
         Bdims2 in ((), (4,)),
         Tt in Tset,
@@ -16,13 +16,35 @@ Tdouble = (Float64, ComplexF64)
         shift in (true, false)
 
         t = tscale * randn(Tt)
-        A = MT(randn(TA, n, n))
+        if MT <: Bidiagonal
+            A = MT(randn(TA, n, n), :U)
+        else
+            A = MT(randn(TA, n, n))
+        end
         B = randn(TB, n, Bdims2...)
         T = Base.promote_eltype(t, A, B)
         rT = real(T)
         @inferred expv(t, A, B; shift=shift)
-        @test expv(t, A, B; shift=shift) ≈ exp(t * A) * B
-        @test expv(t, A, B; shift=shift) ≈ exp(t * A) * B
+        @test expv(t, A, B; shift=shift) ≈ exp(Matrix(t * A)) * B
+        @test eltype(expv(t, A, B; shift=shift)) === T
+    end
+
+    @testset "expv(t::$Tt, A::SparseMatrixCSC{$TA}, B::Array{$TB,$(length(Bdims2)+1)}), tscale=$tscale, shift=$shift" for
+        Tset in (Tsingle, Tdouble),
+        Bdims2 in ((), (4,)),
+        Tt in Tset,
+        TA in Tset,
+        TB in Tset,
+        tscale in Tt.((0.1, 1, 10)),
+        shift in (true, false)
+
+        t = tscale * randn(Tt)
+        A = sprandn(TA, n, n, 1 / n)
+        B = randn(TB, n, Bdims2...)
+        T = Base.promote_eltype(t, A, B)
+        rT = real(T)
+        @inferred expv(t, A, B; shift=shift)
+        @test expv(t, A, B; shift=shift) ≈ exp(Matrix(t * A)) * B
         @test eltype(expv(t, A, B; shift=shift)) === T
     end
 
