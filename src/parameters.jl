d129aff87c27fcb7a859b4b90a3432701ae2ffd8
Seth Axen <seth.axen@gmail.com>
2021-03-17 18:03:00 -0700

    Choose coefficients using higher precision math

--- src/parameters.jl
+++ src/parameters.jl
@@ -8,11 +8,12 @@ This is Code Fragment 3.1 from [^AlMohyHigham2011].
 function parameters(
     t, A, n0, m_max, p_max=p_from_m(m_max), tol=eps(float(real(Base.promote_eltype(t, A))))
 )
-    tnorm = abs(t)
     θ = coefficients(tol)
     T = float(Base.promote_eltype(t, A, θ))
+    Tr = real(T)
+    tnorm = Tr(abs(t))
     iszero(tnorm) && return (m=0, s=1)
-    Anorm = opnormest1(A)
+    Anorm = Tr(opnormest1(A))
     iszero(Anorm) && return (m=0, s=1)
     tAnorm = tnorm * Anorm
     ℓ = 2 # §3: “where the positive integer ℓ is a parameter (typically set to 1 or 2)”
@@ -32,13 +33,13 @@ function parameters(
         # TODO: replace powers of A here and below with opnormest(pow, A, 1)
         # see https://github.com/JuliaLang/julia/pull/39058
         Apow = A * A
-        d = tnorm * sqrt(opnormest1(A))
+        d = tnorm * sqrt(Tr(opnormest1(A)))
         m_opt = m_max + 1
         Cm_opt = typemax(Int)
         for p in 2:p_max # Compute minimum in (3.11)
             Apow *= A
             # (3.7)
-            d, d_old = tnorm * opnormest1(Apow)^(1//(p + 1)), d
+            d, d_old = tnorm * Tr(opnormest1(Apow))^(1//(p + 1)), d
             α = max(d, d_old)
             m_min = p * (p - 1) - 1
             # work around argmin not taking a function
