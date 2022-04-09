9f74b0ac3a24b641265540efb790ffab0b1f3869
Seth Axen <seth.axen@gmail.com>
2022-04-10 01:49:50 +0200

    Add expv_sequence for interval

--- src/sequence.jl
+++ src/sequence.jl
@@ -16,6 +16,12 @@ point.
 
 See [`expv`](@ref) for a description of acceptable `kwargs`.
 
+    expv_sequence(ts::AbstractRange, A, B; kwargs...)
+
+Compute `expv` over the uniformly spaced sequence.
+
+This algorithm takes special care to avoid overscaling and is described in Code Fragment 5.1
+of [^AlMohyHigham2011].
 """
 function expv_sequence(ts, A, B; kwargs...)
     F = expv(ts[begin], A, B; kwargs...)
@@ -26,5 +32,44 @@ function expv_sequence(ts, A, B; kwargs...)
         F = expv(Δt, A, F; kwargs...)
         Fs[i] = F
     end
+    return Fs
+end
+function expv_sequence(ts::AbstractRange, A, B; shift=true, tol=default_tol(ts, A, B))
+    n = LinearAlgebra.checksquare(A)
+    ncols_B = size(B, 2)
+    num_steps = length(ts) - 1  # q
+    t_min, t_max = extrema(ts)
+    t_span = t_max - t_min
+    Δt = step(ts)
+
+    if shift
+        μ = tr(A) / n
+        A -= μ * I
+    else
+        μ = zero(float(eltype(A)))
+    end
+
+    F = expv(t_min, A, B; shift=false, tol) * exp(μ * t_min)
+    Fs = Vector{typeof(F)}(undef, num_steps + 1)
+    Fs[1] = F
+
+    degree_opt, scale = parameters(t_span, A, ncols_B; tol)  # (m*, s)
+    num_steps_per_scale = fld(num_steps, scale)  # d
+    scale_actual, num_steps_last = fldmod(num_steps, num_steps_per_scale)  # (j, r)
+
+    Z = F
+    l = 1
+    for i in 1:scale_actual
+        if i > scale_actual
+            num_steps_per_scale = num_steps_last
+        end
+        for k in 1:num_steps_per_scale
+            l += 1
+            η = exp(μ * k * Δt)
+            Fs[l] = F = expv_taylor(k * Δt, A, Z, degree_opt; tol) * η
+        end
+        Z = F
+    end
+
     return Fs
 end
